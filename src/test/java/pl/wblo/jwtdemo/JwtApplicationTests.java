609e3f3647c46db999d7f492d3987fcfa7ca4b98
Stacja <fafassf@dfasdfasdfasdfasdfadfadfasdfafdafd.pl>
2025-09-02 01:44:32 +0200

    Runtime problems - bez code review
    # Conflicts:
    #	.mvn/wrapper/maven-wrapper.properties
    #	pom.xml

---
+++ src/test/java/pl/wblo/jwtdemo/JwtApplicationTests.java
@@ -0,0 +1,12 @@
+package pl.wblo.jwtdemo;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
